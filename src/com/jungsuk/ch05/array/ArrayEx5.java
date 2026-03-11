5acc03678b3f96bf99002f52f0fdc509cbd8378a
rlarkawk2 <229rkawk@gamil.com>
2026-03-11 21:23:23 +0900

    ArrayEx5 배열 총합, 평균

---
+++ src/com/jungsuk/ch05/array/ArrayEx5.java
@@ -0,0 +1,22 @@
+package com.jungsuk.ch05.array;
+
+import java.util.Scanner;
+
+public class ArrayEx5 {
+	public static void main(String[] args) {
+		/*
+		 * 예제5-5. 총합과 평균
+		 * 배열의 모든 요소의 총합과 평균을 구함
+		 */
+		int sum = 0;
+		int[] arr = new int[] {1,2,3,4,5};
+
+		for(int i:arr)
+		{
+			sum += i;
+		}
+
+		System.out.printf("총합 : %d\n", sum);
+		System.out.printf("평균 : %f", (float)sum/arr.length);
+	}
+}
